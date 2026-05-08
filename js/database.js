592216e541a97e03260a5184ff43da7f6c569ac7
hutorokfermakz <hutor3671@gmail.com>
2026-05-09 02:19:52 +0500

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,1 +1,35 @@
+window.DB = {
 
+  async getPlayer(id) {
+
+    const { data, error } = await supabaseClient
+      .from("players")
+      .select("*")
+      .eq("telegram_id", id)
+      .single();
+
+    if (error) return null;
+
+    return data;
+  },
+
+  async createPlayer(player) {
+
+    const { data, error } = await supabaseClient
+      .from("players")
+      .insert([player]);
+
+    return data;
+  },
+
+  async savePlayer(id, updates) {
+
+    const { data, error } = await supabaseClient
+      .from("players")
+      .update(updates)
+      .eq("telegram_id", id);
+
+    return data;
+  }
+
+};
