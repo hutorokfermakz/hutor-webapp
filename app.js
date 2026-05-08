956c6ead43857af11b4a9f1d14b852427e056194
hutorokfermakz <hutor3671@gmail.com>
2026-05-08 17:35:36 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("HUTOR WEBAPP STARTED");
+let coins = 0;
+
+const coinsText = document.getElementById("coins");
+const farmBtn = document.getElementById("farmBtn");
+
+farmBtn.onclick = () => {
+  coins += 10;
+  coinsText.innerText = coins + " монет";
+};
