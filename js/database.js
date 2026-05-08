e780602015a2bb7aff2a2f4a14577e9d4428b0d6
hutorokfermakz <hutor3671@gmail.com>
2026-05-09 03:19:04 +0500

    Update database.js

--- js/database.js
+++ js/database.js
@@ -1,29 +1,43 @@
+const tg = window.Telegram.WebApp
+tg.expand()
+
 async function loadPlayer() {
-  const tg = window.Telegram.WebApp
+
   const user = tg.initDataUnsafe.user
 
-  if (!user) return
+  if (!user) {
+    alert("Telegram user not found")
+    return
+  }
 
-  let { data } = await supabase
+  console.log(user)
+
+  let { data, error } = await supabase
     .from("players")
     .select("*")
     .eq("telegram_id", user.id)
     .single()
 
-  if (!data) {
-    const { data: newPlayer } = await supabase
-      .from("players")
-      .insert({
-        telegram_id: user.id,
-        name: user.first_name,
-        coins: 500,
-        level: 1,
-        xp: 0
-      })
-      .select()
-      .single()
+  console.log(data, error)
 
-    data = newPlayer
+  if (!data) {
+
+    const { data: newPlayer, error: insertError } = await supabase
+      .from("players")
+      .insert([
+        {
+          telegram_id: user.id,
+          name: user.first_name,
+          coins: 500,
+          level: 1,
+          xp: 0
+        }
+      ])
+      .select()
+
+    console.log(newPlayer, insertError)
+
+    data = newPlayer[0]
   }
 
   document.getElementById("playerName").innerText = data.name
