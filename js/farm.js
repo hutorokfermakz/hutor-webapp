2eb88c9265d88da340e087d03e9757c83034bb0d
hutorokfermakz <hutor3671@gmail.com>
2026-05-09 04:18:46 +0500

    Update farm.js

--- js/farm.js
+++ js/farm.js
@@ -1,1 +1,15 @@
+const tiles = document.querySelectorAll(".farm-tile")
 
+tiles.forEach(tile => {
+
+  tile.addEventListener("click", async () => {
+
+    const tileIndex = tile.dataset.tile
+
+    console.log("Tile:", tileIndex)
+
+    tile.innerHTML = "🌾"
+
+  })
+
+})
