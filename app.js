f991728327f5a63769cfdeac46c78d47944a2ad0
hutorokfermakz <hutor3671@gmail.com>
2026-05-08 20:50:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,58 +1,20 @@
+alert("JS WORKING");
+
 document.addEventListener("DOMContentLoaded", () => {
 
-  const tg = window.Telegram.WebApp;
-
-  tg.expand();
-
-  let money = 500;
-  let level = 1;
-  let xp = 0;
-
-  const moneyEl = document.getElementById("money");
-  const levelEl = document.getElementById("level");
-  const xpFill = document.getElementById("xpFill");
+  alert("DOM LOADED");
 
   const farmBtn = document.getElementById("farmBtn");
-  const caseBtn = document.getElementById("caseBtn");
 
-  function updateUI() {
-
-    moneyEl.innerText = money;
-    levelEl.innerText = level;
-
-    xpFill.style.width = xp + "%";
+  if (!farmBtn) {
+    alert("Кнопка не найдена");
+    return;
   }
 
+  alert("Кнопка найдена");
+
   farmBtn.addEventListener("click", () => {
-
-    tg.HapticFeedback.impactOccurred("light");
-
-    money += 10;
-    xp += 10;
-
-    if (xp >= 100) {
-      level++;
-      xp = 0;
-
-      alert("🎉 Новый уровень!");
-    }
-
-    updateUI();
+    alert("КНОПКА РАБОТАЕТ");
   });
-
-  caseBtn.addEventListener("click", () => {
-
-    tg.HapticFeedback.notificationOccurred("success");
-
-    const reward = Math.floor(Math.random() * 100) + 50;
-
-    money += reward;
-
-    alert("🎁 +" + reward + " монет");
-
-    updateUI();
-  });
-
-  updateUI();
 
 });
