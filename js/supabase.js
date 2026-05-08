0139f86ddd9b998a19dcec74fbaae85d5d0fdae6
hutorokfermakz <hutor3671@gmail.com>
2026-05-09 02:19:29 +0500

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,9 @@
+const SUPABASE_URL = "https://gihybzpefojxiyyxheks.supabase.co";
 
+const SUPABASE_KEY =
+"sb_publishable_epzMrCasnlXMmAENesXgTw_dkRzwBag";
+
+window.supabaseClient = supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
