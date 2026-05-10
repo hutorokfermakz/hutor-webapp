1f8daafd18116740d966ec95dcea9d57d749652e
hutorokfermakz <hutor3671@gmail.com>
2026-05-10 12:04:59 +0500

    Update crops.js

--- js/systems/crops.js
+++ js/systems/crops.js
@@ -1,1 +1,71 @@
+window.CROPS = {
 
+  wheat: {
+
+    id: "wheat",
+
+    name: "Пшеница",
+
+    icon: "🌾",
+
+    growTime: 10000,
+
+    reward: 20,
+
+    xp: 10,
+
+    level: 1
+  },
+
+  carrot: {
+
+    id: "carrot",
+
+    name: "Морковь",
+
+    icon: "🥕",
+
+    growTime: 30000,
+
+    reward: 50,
+
+    xp: 20,
+
+    level: 2
+  },
+
+  corn: {
+
+    id: "corn",
+
+    name: "Кукуруза",
+
+    icon: "🌽",
+
+    growTime: 60000,
+
+    reward: 90,
+
+    xp: 35,
+
+    level: 3
+  },
+
+  tomato: {
+
+    id: "tomato",
+
+    name: "Томаты",
+
+    icon: "🍅",
+
+    growTime: 120000,
+
+    reward: 150,
+
+    xp: 60,
+
+    level: 5
+  }
+
+};
