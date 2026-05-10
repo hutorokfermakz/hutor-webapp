6d35322add17cd7b3f558690e94a5e9a1a4dc998
hutorokfermakz <hutor3671@gmail.com>
2026-05-10 20:49:55 +0500

    Update inventory.js

--- js/systems/inventory.js
+++ js/systems/inventory.js
@@ -1,1 +1,22 @@
+window.addItem =
+function(item, amount=1) {
 
+  const found =
+  gameState.inventory.find(
+    i => i.item === item
+  );
+
+  if(found) {
+
+    found.amount += amount;
+
+  } else {
+
+    gameState.inventory.push({
+      item,
+      amount
+    });
+
+  }
+
+};
