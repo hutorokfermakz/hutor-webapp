1d8751d06b8def75b0f1df4cd39c35129e7f7654
hutorokfermakz <hutor3671@gmail.com>
2026-05-10 12:28:15 +0500

    Update notifications.js

--- js/ui/notifications.js
+++ js/ui/notifications.js
@@ -1,1 +1,31 @@
-console.log("Notifications loaded");
+function showNotification(text){
+
+  const div =
+  document.createElement("div");
+
+  div.className =
+  "game-notification";
+
+  div.innerText = text;
+
+  document.body.appendChild(div);
+
+  setTimeout(()=>{
+
+    div.classList.add("show");
+
+  },100);
+
+  setTimeout(()=>{
+
+    div.classList.remove("show");
+
+    setTimeout(()=>{
+
+      div.remove();
+
+    },300);
+
+  },2500);
+
+}
