a2592172dbbf1bca2d05394497143d99883b0741
hutorokfermakz <hutor3671@gmail.com>
2026-05-10 12:26:27 +0500

    Update levels.js

--- js/systems/levels.js
+++ js/systems/levels.js
@@ -1,1 +1,45 @@
+function getLevelXp(level){
 
+  return level * 100;
+
+}
+
+async function checkLevelUp(){
+
+  const player =
+  window.gameState.player;
+
+  let neededXp =
+  getLevelXp(player.level);
+
+  // LEVEL UP
+
+  while(player.xp >= neededXp){
+
+    player.xp -= neededXp;
+
+    player.level += 1;
+
+    neededXp =
+    getLevelXp(player.level);
+
+    showNotification(
+      "🎉 Новый уровень: " +
+      player.level
+    );
+
+  }
+
+  // SAVE
+
+  await window.supabaseClient
+    .from("players")
+    .update({
+      level: player.level,
+      xp: player.xp
+    })
+    .eq("id", player.id);
+
+  updatePlayerUI(player);
+
+}
