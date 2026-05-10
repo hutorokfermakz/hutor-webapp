56ab38ade594949a90053a19d3075018bb653e59
hutorokfermakz <hutor3671@gmail.com>
2026-05-10 20:52:53 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
+createFarm();
 
+gameState.player =
+createPlayer();
+
+document.getElementById(
+  "playerName"
+).innerText =
+gameState.player.username;
+
+document.getElementById(
+  "coins"
+).innerText =
+gameState.player.coins;
+
+renderFarm();
+
+updateWeather();
+
+console.log(
+  "HUTOROK STARTED"
+);
