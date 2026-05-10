29b3c1ebdb085185298b9f427911a3ccac5c479a
hutorokfermakz <hutor3671@gmail.com>
2026-05-10 20:51:23 +0500

    Update weather.js

--- js/systems/weather.js
+++ js/systems/weather.js
@@ -1,1 +1,23 @@
+window.updateWeather =
+function() {
 
+  const weather =
+  [
+    "☀️ Солнечно",
+    "🌧 Дождь",
+    "⛈ Шторм"
+  ];
+
+  const random =
+  weather[
+    Math.floor(
+      Math.random() *
+      weather.length
+    )
+  ];
+
+  document.getElementById(
+    "weatherBar"
+  ).innerHTML = random;
+
+};
