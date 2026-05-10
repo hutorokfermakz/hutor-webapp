a35e06f39cbdd2d9ffccc50aa68b4b7d677dcfe8
hutorokfermakz <hutor3671@gmail.com>
2026-05-10 11:13:57 +0500

    Update navigation.js

--- js/core/navigation.js
+++ js/core/navigation.js
@@ -1,1 +1,34 @@
+const navButtons =
+document.querySelectorAll(".nav-btn");
 
+const pages =
+document.querySelectorAll(".page");
+
+navButtons.forEach(btn => {
+
+  btn.addEventListener("click", () => {
+
+    // ACTIVE BUTTON
+
+    navButtons.forEach(b => {
+      b.classList.remove("active");
+    });
+
+    btn.classList.add("active");
+
+    // ACTIVE PAGE
+
+    const pageId =
+    btn.dataset.page;
+
+    pages.forEach(page => {
+      page.classList.remove("active");
+    });
+
+    document
+      .getElementById(pageId)
+      .classList.add("active");
+
+  });
+
+});
