131bd986dce644a9f61102bdca0f65f6c8811a41
hutorokfermakz <hutor3671@gmail.com>
2026-05-11 13:19:13 +0500

    Update router.js

--- js/core/router.js
+++ js/core/router.js
@@ -1,1 +1,39 @@
+export function initRouter() {
 
+  const navButtons =
+    document.querySelectorAll(".nav-btn");
+
+  navButtons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+      const target =
+        button.dataset.screen;
+
+      switchScreen(target);
+
+    });
+
+  });
+
+}
+
+export function switchScreen(screen) {
+
+  document
+    .querySelectorAll(".screen")
+    .forEach((screenEl) => {
+
+      screenEl.classList.remove(
+        "active"
+      );
+
+    });
+
+  document
+    .getElementById(
+      `${screen}-screen`
+    )
+    .classList.add("active");
+
+}
