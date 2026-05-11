5b678feb58e89e1bc3616c4babebfa89e012edd8
hutorokfermakz <hutor3671@gmail.com>
2026-05-11 13:24:42 +0500

    Update config.js

--- js/core/config.js
+++ js/core/config.js
@@ -1,1 +1,43 @@
+export const CONFIG = {
 
+  crops: {
+
+    wheat: {
+      id: "wheat",
+      name: "Пшеница",
+
+      seedPrice: 25,
+      reward: 60,
+
+      growTime: 2500,
+
+      rarity: "common"
+    },
+
+    corn: {
+      id: "corn",
+      name: "Кукуруза",
+
+      seedPrice: 40,
+      reward: 90,
+
+      growTime: 3000,
+
+      rarity: "common"
+    },
+
+    carrot: {
+      id: "carrot",
+      name: "Морковь",
+
+      seedPrice: 60,
+      reward: 140,
+
+      growTime: 3500,
+
+      rarity: "rare"
+    }
+
+  }
+
+};
