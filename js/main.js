aab13a5ceb2b3ecfd455370eb70ecb3aeef6fdb7
hutorokfermakz <hutor3671@gmail.com>
2026-05-11 13:20:12 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+import { initRouter }
+from "./core/router.js";
 
+import { renderApp }
+from "./core/renderer.js";
+
+function bootGame() {
+
+  initRouter();
+
+  renderApp();
+
+  console.log(
+    "HUTOROK v7 STARTED"
+  );
+
+}
+
+bootGame();
