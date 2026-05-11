b711ec8445bcd986174ee9974e7e51d33f7a9d93
hutorokfermakz <hutor3671@gmail.com>
2026-05-11 13:17:33 +0500

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -1,1 +1,33 @@
+export const gameState = {
 
+  player: {
+    coins: 500,
+    crystals: 0,
+
+    level: 1,
+    xp: 0,
+    xpToNext: 100
+  },
+
+  farm: {
+    weather: "rain",
+    time: "night",
+
+    plots: Array(8).fill(null)
+  },
+
+  inventory: {
+    selectedSeed: null,
+
+    items: [
+      {
+        id: "wheat_seed",
+        type: "seed",
+        name: "Пшеница",
+        amount: 5,
+        rarity: "common"
+      }
+    ]
+  }
+
+};
