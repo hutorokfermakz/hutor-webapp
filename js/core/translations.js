f409518aa9bc47a415a94ee48647dfb3814ad70e
hutorokfermakz <hutor3671@gmail.com>
2026-05-11 13:16:44 +0500

    Update translations.js

--- js/core/translations.js
+++ js/core/translations.js
@@ -1,1 +1,16 @@
+export const TEXT = {
 
+  farm: "Ферма",
+  inventory: "Инвентарь",
+  shop: "Магазин",
+  clans: "Кланы",
+  chat: "Чат",
+  profile: "Профиль",
+
+  harvest: "Собрать",
+  plant: "Посадить",
+
+  coins: "Монеты",
+  crystals: "Кристаллы"
+
+};
