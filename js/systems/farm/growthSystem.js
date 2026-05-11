a0f0b7c5845ba998dd372651cc360f573bc79c80
hutorokfermakz <hutor3671@gmail.com>
2026-05-11 15:38:38 +0500

    Update growthSystem.js

--- js/systems/farm/growthSystem.js
+++ js/systems/farm/growthSystem.js
@@ -1,1 +1,28 @@
+import { renderPlots }
+from "./plotRenderer.js";
 
+export function growCrop(plot) {
+
+  plot.stage = 1;
+
+  renderPlots();
+
+  setTimeout(() => {
+
+    plot.stage = 2;
+
+    renderPlots();
+
+  }, 2500);
+
+  setTimeout(() => {
+
+    plot.stage = 3;
+
+    plot.ready = true;
+
+    renderPlots();
+
+  }, 5000);
+
+}
