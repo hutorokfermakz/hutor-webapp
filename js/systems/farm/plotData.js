fe7de59843951d7f5343d3e86ca14fd1e775eabd
hutorokfermakz <hutor3671@gmail.com>
2026-05-11 15:00:33 +0500

    Create plotData.js

---
+++ js/systems/farm/plotData.js
@@ -0,0 +1,45 @@
+export const plots = [
+
+  {
+    id: 1,
+    planted: false,
+    crop: null,
+    stage: 0
+  },
+
+  {
+    id: 2,
+    planted: false,
+    crop: null,
+    stage: 0
+  },
+
+  {
+    id: 3,
+    planted: false,
+    crop: null,
+    stage: 0
+  },
+
+  {
+    id: 4,
+    planted: false,
+    crop: null,
+    stage: 0
+  },
+
+  {
+    id: 5,
+    planted: false,
+    crop: null,
+    stage: 0
+  },
+
+  {
+    id: 6,
+    planted: false,
+    crop: null,
+    stage: 0
+  }
+
+];
