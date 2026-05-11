e40a524ef4d2d8f0005d3e726deec71d7c035d37
hutorokfermakz <hutor3671@gmail.com>
2026-05-11 15:52:21 +0500

    Update growthSystem.js

--- js/systems/farm/growthSystem.js
+++ js/systems/farm/growthSystem.js
@@ -1,28 +1,35 @@
-import { renderPlots }
-from "./plotRenderer.js";
+import { renderPlots } from "./plotRenderer.js";
 
-export function growCrop(plot) {
+export function growCrop(plot, crop) {
+  if (!plot || !crop) return;
+
+  plot.growing = true;
+
+  const total = crop.growTime;
+
+  const stageOne = total * 0.3;
+  const stageTwo = total * 0.65;
 
   plot.stage = 1;
 
   renderPlots();
 
   setTimeout(() => {
-
     plot.stage = 2;
-
     renderPlots();
-
-  }, 2500);
+  }, stageOne);
 
   setTimeout(() => {
-
     plot.stage = 3;
+    renderPlots();
+  }, stageTwo);
+
+  setTimeout(() => {
+    plot.stage = 4;
 
     plot.ready = true;
+    plot.growing = false;
 
     renderPlots();
-
-  }, 5000);
-
+  }, total);
 }
