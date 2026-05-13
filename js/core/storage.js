7b476303ff2be8457a74b610001142d9749c07b5
hutorokfermakz <hutor3671@gmail.com>
2026-05-13 12:15:01 +0500

    Update storage.js

--- js/core/storage.js
+++ js/core/storage.js
@@ -1,1 +1,55 @@
+/* =========================================
+   STORAGE SYSTEM
+========================================= */
 
+const STORAGE_KEY =
+    "hutorok_save";
+
+/* SAVE GAME */
+
+export function saveGame(data) {
+
+    localStorage.setItem(
+
+        STORAGE_KEY,
+
+        JSON.stringify(data)
+
+    );
+
+}
+
+/* LOAD GAME */
+
+export function loadGame() {
+
+    const save =
+        localStorage.getItem(STORAGE_KEY);
+
+    if (!save) {
+
+        return null;
+
+    }
+
+    try {
+
+        return JSON.parse(save);
+
+    } catch {
+
+        return null;
+
+    }
+
+}
+
+/* CLEAR SAVE */
+
+export function clearSave() {
+
+    localStorage.removeItem(
+        STORAGE_KEY
+    );
+
+}
